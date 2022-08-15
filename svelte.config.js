999d6cac6881bd591760d2102f4aa31fddbf92d7
Ken Mueller <kenmueller0@gmail.com>
2022-08-15 10:29:37 -0700

    unsafe inline script

--- svelte.config.js
+++ svelte.config.js
@@ -34,6 +34,7 @@ const config = {
 			directives: {
 				'base-uri': ['self'],
 				'default-src': ['self'],
+				'script-src': ['self', 'unsafe-inline'],
 				'style-src': ['self', 'unsafe-inline']
 			}
 		}
