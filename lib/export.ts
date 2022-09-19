c7d148aa7f66b154b25bfa0ce866fbf4fec50145
Ken Mueller <kenmueller0@gmail.com>
2022-09-19 15:12:36 -0700

    style rectangle

--- lib/export.ts
+++ lib/export.ts
@@ -10,16 +10,14 @@ const exportDocument = (
 	return `\\usepackage{tikz}
 \\usetikzlibrary{arrows}
 
-\\begin{tikzpicture}[->, >=stealth', thick, every node/.style={circle, fill, inner sep=1.5pt}]
+\\begin{tikzpicture}[->, >=stealth', thick, every node/.style={circle, fill, inner sep=1.5pt}, every label/.append style={rectangle}]
 ${Object.entries(nodes)
 	.map(
 		([id, node]) =>
 			`\t\\node at (${[
 				nearestDivisor(node.x, GRID_SPACING),
 				nearestDivisor(node.y, GRID_SPACING)
-			].join(', ')}) (${id}) [label={[label distance=-3mm]above:{${
-				node.name
-			}}}]{};`
+			].join(', ')}) (${id}) [label=above:{${node.name}}]{};`
 	)
 	.join('\n')}
 ${arrows.map(arrow => `\t\\draw (${arrow.from}) -- (${arrow.to});`).join('\n')}
