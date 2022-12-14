d9ae0d5cffca5c2d98a1f8de0d5f81b4ca09d439
Ken Mueller <kenmueller0@gmail.com>
2022-12-14 14:45:09 -0500

    rotation

--- lib/export.ts
+++ lib/export.ts
@@ -3,6 +3,19 @@ import type Arrow from './arrow'
 import nearestDivisor from './nearest/divisor'
 import GRID_SPACING from './grid/spacing'
 
+const rotationToLabelPosition = (rotation: 0 | 90 | 180 | 270) => {
+	switch (rotation) {
+		case 0:
+			return 'above'
+		case 90:
+			return 'right'
+		case 180:
+			return 'below'
+		case 270:
+			return 'left'
+	}
+}
+
 const exportDocument = (
 	nodes: Record<string, Node>,
 	arrows: Arrow<string>[]
@@ -17,7 +30,9 @@ ${Object.entries(nodes)
 			`\t\\node at (${[
 				nearestDivisor(node.x, GRID_SPACING),
 				nearestDivisor(node.y, GRID_SPACING)
-			].join(', ')}) (${id}) [label=above:{${node.name}}]{};`
+			].join(', ')}) (${id}) [label=${rotationToLabelPosition(
+				node.rotation
+			)}:{${node.name}}]{};`
 	)
 	.join('\n')}
 ${arrows.map(arrow => `\t\\draw (${arrow.from}) -- (${arrow.to});`).join('\n')}
